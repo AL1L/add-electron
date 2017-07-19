99cda01791eb0007e8c0d1c217a0bc2faf04f414
mathhulk <contact@theartex.net>
2017-07-19 10:15:28 -0700

    First build

---
+++ assets/js/add.js
@@ -0,0 +1,323 @@
+const app = require('electron');
+const path = require('path');
+const url = require('url');
+const storage = require('electron-json-storage');
+const request = require('request');
+const dateFormat = require('dateformat');
+
+/*
+ *	AUTHENTICATION
+ */
+function authCheck() {
+	storage.get('auth', function(error, data) {
+		if (error) throw error;
+	 
+		if(data.token && data.id) {
+			request({
+				url: "https://www.theartex.net/cloud/api/",
+				method: "POST",
+				json: true,
+				body: {sec: "validate", id: data.id, token: data.token}
+			}, function (error, response, body) {
+				if (error) throw error;
+				
+				if(response.body.status == "success") {
+					$.each(response.body.data, function(key, value) {
+						$("." + key).text(value);
+					});
+					storage.set('user', {username: response.body.data.username, role: response.body.data.role, email: response.body.data.email, gravatar: response.body.data.gravatar, key: response.body.data.key, page: response.body.data.page, last_seen: response.body.data.last_seen}, function(error) {
+						if (error) throw error;
+					});
+				} else {
+					storage.clear(function(error) {
+						if (error) throw error;
+					});
+					app.remote.getCurrentWindow().loadURL('https://www.theartex.net/system/login/?red=/' + __dirname + '/index.html&prot=file&data=get');
+				}
+			});
+		} else {
+			storage.clear(function(error) {
+				if (error) throw error;
+			});
+			app.remote.getCurrentWindow().loadURL('https://www.theartex.net/system/login/?red=/' + __dirname + '/index.html&prot=file&data=get');
+		}
+	});
+	
+	storage.get('user', function(error, data) {
+		if (error) throw error;
+		
+		request({
+			url: "https://www.theartex.net/cloud/api/",
+			method: "POST",
+			json: true,
+			body: {sec: "alerts", key: data.key}
+		}, function (error, response, body) {
+			if (error) throw error;
+			
+			if(response.body.status == "success") {
+				var message;
+				
+				$.each(response.body.data, function(key, value) {
+					if(value.status == "new") {
+						message = value.message;
+						
+						if(message.length > 200) {
+							message = message.substr(0, 197) + "...";
+						}
+						
+						let myNotification = new Notification(value.title, {body: message})
+						
+						request({
+							url: "https://www.theartex.net/cloud/api/",
+							method: "POST",
+							json: true,
+							body: {sec: "status", id: value.id, status: "idle", key: data.key}
+						}, function (error) {
+							if (error) throw error;
+						});
+					}
+				});
+			}
+		});
+	});
+}
+
+/*
+ *	EVENTS
+ */
+$(".minimizeWindow").click(function() {
+    app.remote.getCurrentWindow().minimize();
+});
+
+$(".maximizeWindow").click(function() {
+    (!app.remote.getCurrentWindow().isMaximized() ? app.remote.getCurrentWindow().maximize() : app.remote.getCurrentWindow().unmaximize());
+});
+
+$(".closeWindow").click(function() {
+    app.remote.getCurrentWindow().close();
+});
+
+$(".getAnnouncements").click(function() {
+    getAnnouncements();
+});
+
+$(".getAlerts").click(function() {
+    getAlerts();
+});
+
+$(".authOut").click(function() {
+	storage.clear(function(error) {
+		if (error) throw error;
+	});
+	app.remote.getCurrentWindow().loadURL('https://www.theartex.net/system/login/?red=/' + __dirname + '/index.html&prot=file&data=get');
+});
+
+$(document).on('click', 'a[href^="http"]', function(event) {
+    event.preventDefault();
+    app.shell.openExternal(this.href);
+});
+
+/*
+ *	CONTENT
+ */
+$(".pagination").on('click', 'a', function() {
+	if($('.content').attr('data-page') == 'alerts') {
+		getAlerts(parseInt($(this).attr("data-page")));
+	} else {
+		getAnnouncements(parseInt($(this).attr("data-page")));
+	}
+});
+
+/*
+ *	ANNOUNCEMENTS
+ */
+function getAnnouncements(page = 1, multiple = 10) {
+	$('.content').attr('data-page', 'announcements');
+	
+	$('.pagination').html('');
+	$('tbody').html('');
+	$('thead').html('');
+	
+	$('thead').append('<tr><th>Announcement</th><th class="text-right">Date</th></tr>');
+	$('tbody').append('<tr><td>Loading announcements...</td><td class="text-right">---</td></tr>');
+	
+	var page = {min: (page - 1) * multiple, max: page * multiple, count: 0, posts: 0, number: page};
+	var pages = {};
+	
+	request({
+		url: "https://www.theartex.net/cloud/api/",
+		method: "POST",
+		json: true,
+		body: {sec: "announcements"}
+	}, function (error, response, body) {
+		if (error) throw error;
+		
+		$('tbody').html('');
+		
+		if(response.body.status == "success") {
+			$.each(response.body.data, function(key, value) {
+				page.count++;
+				
+				if(page.count <= page.max && page.count > page.min) {
+					$('tbody').append('<tr><td>' + value.message + '</td><td class="text-right">' + dateFormat(value.trn_date, 'mmmm dS, yyyy') + '</td></tr>');
+					page.posts++;
+				}
+			});
+			
+			if(page.posts == 0) {
+				$('tbody').append('<tr><td>No announcements could be listed on this page.</td><td class="text-right">---</td></tr>');
+			} 
+			
+			if(page.number > 1) {
+				$('.pagination').append('<a data-page="1"><i class="fa fa-angle-double-left"></i> First</a><a data-page="' + (page.number - 1) + '"><i class="fa fa-angle-left"></i> Previous</a>');
+			}
+			
+			var rows = Object.keys(response.body.data).length;
+			pages.total = Math.floor(rows / multiple);
+			
+			if(rows % 10 != 0 || rows == 0) {
+				pages.total++;
+			}
+
+			if(page.number < 5) {
+				pages.min = 1; 
+				pages.max = 9;
+			} else if(page.number > (pages.total - 8)) {
+				pages.min = pages.total - 8; 
+				pages.max = pages.total;
+			} else {
+				pages.min = pages.number - 4; 
+				pages.max = pages.number + 4; 
+			}
+			
+			if(pages.max > pages.total) {
+				pages.max = pages.total;
+			}
+			
+			if(pages.min < 1) {
+				pages.min = 1;
+			}
+			
+			pages.count = pages.min;
+			
+			while(pages.count <= pages.max) {
+				$('.pagination').append('<a' + (pages.count == page.number ? ' class="active"' : '') + ' data-page="' + pages.count + '">' + pages.count + '</a>');
+				pages.count++;
+			}
+			
+			if(rows > page.max) {
+				$('.pagination').append('<a data-page="' + (page.number + 1) + '">Next <i class="fa fa-angle-right"></i></a><a data-page="' + pages.total + '">Last <i class="fa fa-angle-double-right"></i></a>');
+			}
+		} else {
+			$('tbody').append('<tr><td>No announcements could be listed on this page.</td><td class="text-right">---</td></tr>');
+		}
+	});
+}
+
+/*
+ *	ALERTS
+ */
+function getAlerts(page = 1, multiple = 10) {
+	$('.content').attr('data-page', 'alerts');
+	
+	$('.pagination').html('');
+	$('tbody').html('');
+	$('thead').html('');
+	
+	$('thead').append('<tr><th>Alert</th><th class="text-right">Date</th></tr>');
+	$('tbody').append('<tr><td>Loading alerts...</td><td class="text-right">---</td></tr>');
+	
+	var page = {min: (page - 1) * multiple, max: page * multiple, count: 0, posts: 0, number: page};
+	var pages = {};
+	
+	storage.get('user', function(error, data) {
+		if(error) throw error;
+		
+		request({
+			url: "https://www.theartex.net/cloud/api/",
+			method: "POST",
+			json: true,
+			body: {sec: "alerts", key: data.key}
+		}, function (error, response, body) {
+			if (error) throw error;
+			
+			$('tbody').html('');
+			
+			if(response.body.status == "success") {
+				$.each(response.body.data, function(key, value) {
+					page.count++;
+					
+					if(page.count <= page.max && page.count > page.min) {
+						$('tbody').append('<tr><td>' + ((value.status == 'new' || value.status == 'idle') ? '<span class="new"><i class="fa fa-exclamation-triangle"></i></span> ' : '') + '<span class="title">(' + value.title + ')</span> ' + value.message + '</td><td class="text-right">' + dateFormat(value.trn_date, 'mmmm dS, yyyy') + '</td></tr>');
+						page.posts++;
+					}
+					
+					if(value.status == 'new' || value.status == 'idle') {
+						request({
+							url: "https://www.theartex.net/cloud/api/",
+							method: "POST",
+							json: true,
+							body: {sec: "status", id: value.id, status: "old", key: data.key}
+						}, function (error) {
+							if (error) throw error;
+						});
+					}
+				});
+				
+				if(page.posts == 0) {
+					$('tbody').append('<tr><td>No alerts could be listed on this page.</td><td class="text-right">---</td></tr>');
+				} 
+				
+				if(page.number > 1) {
+					$('.pagination').append('<a data-page="1"><i class="fa fa-angle-double-left"></i> First</a><a data-page="' + (page.number - 1) + '"><i class="fa fa-angle-left"></i> Previous</a>');
+				}
+				
+				var rows = Object.keys(response.body.data).length;
+				pages.total = Math.floor(rows / multiple);
+				
+				if(rows % 10 != 0 || rows == 0) {
+					pages.total++;
+				}
+
+				if(page.number < 5) {
+					pages.min = 1; 
+					pages.max = 9;
+				} else if(page.number > (pages.total - 8)) {
+					pages.min = pages.total - 8; 
+					pages.max = pages.total;
+				} else {
+					pages.min = pages.number - 4; 
+					pages.max = pages.number + 4; 
+				}
+				
+				if(pages.max > pages.total) {
+					pages.max = pages.total;
+				}
+				
+				if(pages.min < 1) {
+					pages.min = 1;
+				}
+				
+				pages.count = pages.min;
+				
+				while(pages.count <= pages.max) {
+					$('.pagination').append('<a' + (pages.count == page.number ? ' class="active"' : '') + ' data-page="' + pages.count + '">' + pages.count + '</a>');
+					pages.count++;
+				}
+				
+				if(rows > page.max) {
+					$('.pagination').append('<a data-page="' + (page.number + 1) + '">Next <i class="fa fa-angle-right"></i></a><a data-page="' + pages.total + '">Last <i class="fa fa-angle-double-right"></i></a>');
+				}
+			} else {
+				$('tbody').append('<tr><td>No alerts could be listed on this page.</td><td class="text-right">---</td></tr>');
+			}
+		});
+	});
+}
+
+/*
+ *	PAGE
+ */
+getAnnouncements();
+authCheck();
+setInterval(authCheck, 2500);
---
+++ main.js
@@ -0,0 +1,72 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const storage = require('electron-json-storage');
+
+let authWindow;
+
+function createWindow () {
+	authWindow = new BrowserWindow({width: 800, height: 600, frame: false, show: false, backgroundColor: '#1a1a1a', minWidth: 800, minHeight: 600, webPreferences: {webSecurity: false}});
+	
+	storage.get('auth', function(error, data) {
+		if (error) throw error;
+		
+		if(data.id && data.token && data.remember == "true") {
+			authWindow.loadURL(url.format({
+				pathname: path.join(__dirname, 'index.html'),
+				protocol: 'file:',
+				slashes: true
+			}));
+		} else {
+			storage.clear(function(error) {
+				if (error) throw error;
+			});
+			authWindow.loadURL('https://www.theartex.net/system/login/?red=/' + __dirname + '/index.html&prot=file&data=get');
+		}
+	});
+	
+	authWindow.webContents.on('will-navigate', function (event, newUrl) {
+		event.preventDefault();
+		
+		if(newUrl.includes("?id=") && newUrl.includes("&token=")) {
+			var id = newUrl.split('?')[1].split('&')[0].split('=')[1];
+			var token = newUrl.split('?')[1].split('&')[1].split('=')[1];
+			var remember = newUrl.split('?')[1].split('&')[2].split('=')[1];
+			
+			if(id && token && remember) {
+				storage.set('auth', {id: id, token: token, remember: remember}, function(error) {
+					if (error) throw error;
+				});
+				authWindow.loadURL(url.format({
+					pathname: path.join(__dirname, 'index.html'),
+					protocol: 'file:',
+					slashes: true
+				}));
+			} else {
+				authWindow.loadURL('https://www.theartex.net/system/login/?red=/' + __dirname + '/index.html&prot=file&data=get');
+			}
+		}
+	});
+	
+	authWindow.once('ready-to-show', () => {
+		authWindow.show()
+	});
+
+	authWindow.on('closed', () => {
+		authWindow = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (win === null) {
+		createWindow();
+	}
+});
