9f575ea6d633c434aec33e51ea12f99e988dd412
mathhulk <contact@theartex.net>
2017-08-26 10:22:21 -0700

    Fixed API calls
    Cleaned up app scripts; fixed API calls as per new documentation @
    https://theartex.net/documentation

--- main.js
+++ main.js
@@ -1,71 +1,69 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
-const storage = require('electron-json-storage');
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
+const storage = require("electron-json-storage");
 
-let authWindow;
+let appWindow;
 
+/*
+ *	FUNCTIONS
+ */
 function createWindow () {
-	authWindow = new BrowserWindow({width: 800, height: 600, frame: false, show: false, backgroundColor: '#1a1a1a', minWidth: 800, minHeight: 600, webPreferences: {webSecurity: false}});
-	
-	storage.get('auth', function(error, data) {
+	appWindow = new BrowserWindow({width: 800, height: 600, frame: false, show: false, backgroundColor: "#1a1a1a", minWidth: 800, minHeight: 600, webPreferences: {webSecurity: false}});
+	storage.get("auth", function(error, data) {
 		if (error) throw error;
-		
 		if(data.id && data.token && data.remember == "true") {
-			authWindow.loadURL(url.format({
-				pathname: path.join(__dirname, 'index.html'),
-				protocol: 'file:',
+			appWindow.loadURL(url.format({
+				pathname: path.join(__dirname, "index.html"),
+				protocol: "file:",
 				slashes: true
 			}));
 		} else {
 			storage.clear(function(error) {
 				if (error) throw error;
 			});
-			authWindow.loadURL('https://www.theartex.net/system/login/?red=http://localhost/add-electron');
+			appWindow.loadURL("https://www.theartex.net/system/login/?red=http://localhost/add-electron");
 		}
 	});
 	
-	authWindow.webContents.on('will-navigate', function (event, newUrl) {
+	/*
+	 *	FUNCTION -> EVENTS
+	 */
+	appWindow.webContents.on("will-navigate", function (event, newUrl) {
 		if(newUrl.includes("?id=") && newUrl.includes("&token=")) {
 			event.preventDefault();
-			
-			var id = newUrl.split('?')[1].split('&')[0].split('=')[1];
-			var token = newUrl.split('?')[1].split('&')[1].split('=')[1];
-			var remember = newUrl.split('?')[1].split('&')[2].split('=')[1];
-			
-			if(id && token && remember) {
-				storage.set('auth', {id: id, token: token, remember: remember}, function(error) {
+			if(newUrl.split("?")[1].split("&")[0].split("=")[1] && newUrl.split("?")[1].split("&")[1].split("=")[1] && newUrl.split("?")[1].split("&")[2].split("=")[1]) {
+				storage.set("auth", {id: newUrl.split("?")[1].split("&")[0].split("=")[1], token: newUrl.split("?")[1].split("&")[1].split("=")[1], remember: newUrl.split("?")[1].split("&")[2].split("=")[1]}, function(error) {
 					if (error) throw error;
 				});
-				authWindow.loadURL(url.format({
-					pathname: path.join(__dirname, 'index.html'),
-					protocol: 'file:',
+				appWindow.loadURL(url.format({
+					pathname: path.join(__dirname, "index.html"),
+					protocol: "file:",
 					slashes: true
 				}));
 			} else {
-				authWindow.loadURL('https://www.theartex.net/system/login/?red=http://localhost/add-electron');
+				appWindow.loadURL("https://www.theartex.net/system/login/?red=http://localhost/add-electron");
 			}
 		}
 	});
-	
-	authWindow.once('ready-to-show', () => {
-		authWindow.show()
+	appWindow.once("ready-to-show", () => {
+		appWindow.show()
 	});
-
-	authWindow.on('closed', () => {
-		authWindow = null;
+	appWindow.on("closed", () => {
+		appWindow = null;
 	});
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
+/*
+ *	EVENTS
+ */
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
 		app.quit();
 	}
 });
-
-app.on('activate', () => {
+app.on("activate", () => {
 	if (win === null) {
 		createWindow();
 	}
